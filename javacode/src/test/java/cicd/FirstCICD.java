99d41f82b7300a6f22c1f79dd38c306e677092a9
vipla <vipla@VipsPC>
2024-12-18 20:02:51 +0530

    first commit

---
+++ javacode/src/test/java/cicd/FirstCICD.java
@@ -0,0 +1,12 @@
+package cicd;
+
+public class FirstCICD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Running my first CICD code in jenkins.........");
+	}
+
+}
